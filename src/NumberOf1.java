fe430e94417a5287551e06c85070ac17aa67cc6d
Mereder <550199493@qq.com>
2019-04-09 17:09:23 +0800

    位运算

---
+++ src/NumberOf1.java
@@ -0,0 +1,11 @@
+public class NumberOf1 {
+    public int NumberOf1(int n) {
+        if(n == 0 ) return 0;
+        int count = 0;
+        while(n != 0){
+            n = n&(n-1);
+            count++;
+        }
+        return count;
+    }
+}
