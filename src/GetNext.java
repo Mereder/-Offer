8bd349541d196ebf27dd9c2956d3f5a98828f1b0
Mereder <550199493@qq.com>
2019-05-19 22:35:05 +0800

    二叉树中序遍历结点的下一个节点

---
+++ src/GetNext.java
@@ -0,0 +1,28 @@
+public class GetNext {
+    public class TreeLinkNode{
+        int val;
+        TreeLinkNode left = null;
+        TreeLinkNode right = null;
+        TreeLinkNode next= null;
+
+        public TreeLinkNode(int val) {
+            this.val = val;
+        }
+    }
+    public TreeLinkNode GetNext(TreeLinkNode pNode)
+    {
+        if (pNode == null) return null;
+        if (pNode.right != null){
+            pNode = pNode.right;
+            while (pNode.left!=null){
+                pNode = pNode.left;
+            }
+            return pNode;
+        }
+        while (pNode.next != null){
+            if (pNode.next.left == pNode) return pNode.next;
+            pNode = pNode.next;
+        }
+        return null;
+    }
+}
